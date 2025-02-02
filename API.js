1f29db856cfe68d47f92afc4034eb29782b06b14
Mohamed Essam <114494116+Enmo7@users.noreply.github.com>
2025-02-02 23:06:04 +0200

    Update API.js

--- API.js
+++ API.js
@@ -9,13 +9,13 @@ fetch('https://engtig-iot-ess.hf.space/gradio_api/call/predict', {
 .then(data => {
     console.log("Data from Gradio API:", data);
     // Update your website with the received data
-    $("#heart_rate_value").text(data.heart_rate + " bpm");
-    $("#spo2_value").text(data.spo2 + "%");
-    $("#temperature_value").text(data.temperature + "°C");
-    $("#gsr_value").text(data.gsr + "%");
-    $("#sugar_level_value").text(data.sugar_level + " mg/dL");
-    $("#seizure_prediction_value").text(data.seizure_prediction);
-    $("#fall_prediction_value").text(data.fall_prediction);
+    $("#heart_rate_value").text(data.معدل_ضربات_القلب + " bpm");
+    $("#spo2_value").text(data.نسبة_الأكسجين_في_الدم + "%");
+    $("#temperature_value").text(data.درجة_الحرارة + "°C");
+    $("#gsr_value").text(data.معدل_التعرق + "%");
+    $("#sugar_level_value").text(data.مستوى_السكر + " mg/dL");
+    $("#seizure_prediction_value").text(data.حالة_النوبات);
+    $("#fall_prediction_value").text(data.حالة_السقوط);
 })
 .catch(error => {
     console.error("Error fetching data:", error);
